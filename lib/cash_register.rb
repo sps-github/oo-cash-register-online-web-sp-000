21121c51aeae43ac23ccdd3c14f002da4a6c5a46
Sarah Peters <sarahpieters15@gmail.com>
2021-01-28 16:05:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ class CashRegister
 
   attr_accessor :total, :discount
 
-  def initialize(total = 0, discount = 0)
+  def initialize(total = 0, discount = 20)
     @total = total
     @discount = discount
   end
